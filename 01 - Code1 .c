6c22ce2924b894efb2e5bd7debdf50d25ca77571
Mustafa Mohammed <mustafamohammed0321@gmail.com>
2023-03-21 23:54:41 +0200

    First Commit

---
+++ 01 - Code1 .c
@@ -0,0 +1,16 @@
+/******************************************************/
+/* Author  : Mustafa Muhammed                         */
+/* Date    : 21 Mar 2023                              */
+/* Version : V01                                      */
+/* Hint    : Print A message                          */
+/******************************************************/
+
+#include<stdio.h>
+
+
+int main(void)
+{
+ printf("First repository in sprints\n");
+
+ return 0 ;
+}
